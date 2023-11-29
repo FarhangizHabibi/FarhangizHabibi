3001e9d18be8ba303b511c56f0928ed29f1f3c5c
FarhangizHabibi <150654811+FarhangizHabibi@users.noreply.github.com>
2023-11-29 02:56:18 -0800

    Add files via upload
    Initial commit, project uploaded.

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,20 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/hero_img.png" alt="" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Hi ! i'm Farhangiz Habibi<br /> Versatil full stack developer with experience in web developer data
+            engineering, and machine learning. Eager to leverage skils in a new environment and grow as a developer.
+          </p>
+          
+        </div>
+      </div>
+    </section>
+  );
+}
